e7bd704d6d9feff7a423108ab9f297c7b28b301b
Bin Li <binblee@gmail.com>
2021-01-14 12:57:33 +0800

    project 04, new gitignore

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,76 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@8192
+D=A
+@num_bytes_screen
+M=D             // num_bytes_screen = 8K
+
+@current_color
+M=0             // 0 for white, -1 for black
+
+(LOOP)
+@KBD
+D=M
+@NO_INPUT
+D;JEQ
+
+@keypressed
+M=-1
+@DRAW
+0;JMP
+
+(NO_INPUT)
+@keypressed
+M=0
+
+(DRAW)
+@current_color
+D=M
+@keypressed
+D=D-M
+
+@CONTINUE_KEYBOARD_PROBE
+D;JEQ               // if keypressed == current_color, do nothing
+
+@num_bytes_screen
+D=M
+@count
+M=D                 // set count = 8K
+
+@keypressed
+D=M
+@current_color
+M=D                 // set current_color to value of keypressed, 0 for white, -1 for black
+
+@SCREEN
+D=A
+@current_byte
+M=D                 // screen memory start address
+
+(LOOP_DRAW)
+
+@current_color
+D=M
+
+@current_byte
+A=M
+M=D                 // set color
+
+@current_byte
+M=M+1               // move to next 
+
+@count
+MD=M-1              // count down
+
+@LOOP_DRAW
+D;JGT
+
+(CONTINUE_KEYBOARD_PROBE)
+@LOOP
+0;JMP       // continue keyboard probe loop
+
+(END)
+@END
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,115 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@num_negative
+M=0
+
+@count
+M=0
+
+// if r0 == 0 then
+//    r2 = 0
+//    jmp to end
+// else if r0 < 0 then
+//    num_negative = num_negative + 1, r2=-r0
+// else
+//    r2 = r0
+// end
+
+@R0
+D=M
+@EQ_ZERO
+D;JEQ
+
+@R0_LT_ZERO
+D;JLT
+
+// r0 > 0
+@R0
+D=M
+@R2
+M=D         //r2=r0
+@CHECK_R1
+0;JMP
+
+(R0_LT_ZERO)
+@num_negative
+M=M+1
+@R0
+D=-M
+@R2
+M=D         // r2=r0
+
+// if r1 == 0 then
+//    r2 = 0
+//    jmp to end
+// else if r1 < 0 then
+//   num_negative = num_negative + 1, count=-r1
+// else
+//   count = r1
+// end
+
+(CHECK_R1)
+@R1
+D=M
+@EQ_ZERO
+D;JEQ
+
+@R1_LT_ZERO
+D;JLT
+
+// r1 > 0
+@R1
+D=M
+@count
+M=D         // count=r1
+@LOOP
+0;JMP
+
+(R1_LT_ZERO)
+@num_negative
+M=M+1
+@R1
+D=-M
+@count
+M=D         // count = -r1
+
+// loop while count > 0
+//   r2 = r2 + r0
+(LOOP)
+@count
+MD=M-1
+@NEGATIVE_OR_NOT
+D;JEQ
+@R0
+D=M
+@R2
+M=D+M
+@LOOP
+0;JMP
+
+// if num_negative -1 == 0 then
+//    r2 = -r2
+(NEGATIVE_OR_NOT)
+@num_negative
+D=M
+@1
+D=D&A   // num_negative & 0x01
+@END
+D;JEQ   // if num_negative & 1 == 0, means result is positive number
+
+@R2
+M=-M
+
+@END
+0;JMP
+
+(EQ_ZERO)
+@R2
+M=0
+
+// end of program
+(END)
+@END
+0;JMP      
